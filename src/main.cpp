13a6efcf6aa692ceb2c5268590c3f37ddde20935
Adam Fillion <adamfillion96@gmail.com>
2019-12-27 16:39:10 -0800

    git add, git commit, and git push implemented

--- src/main.cpp
+++ src/main.cpp
@@ -8,56 +8,58 @@
 #include <fstream>
 #include <unordered_set>
 #include <chrono>
+#include <memory.h>
 
 struct Options
 {
   std::string repo;
+  bool online = false; // offline mode - when true, connections to the remote will not be performed.
 };
 
-char* get_current_time()
+char *get_current_time()
 {
   std::time_t time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 
   return std::ctime(&time);
 }
 
-void write_note(const std::string& path, const std::string& note)
-{  
-     std::fstream outfile;
-     outfile.open(path, std::ios::out | std::ios::app);
+void write_note(const std::string &path, const std::string &note)
+{
+  std::fstream outfile;
+  outfile.open(path, std::ios::out | std::ios::app);
 
-     outfile << get_current_time();// time has trailing '\n' built-in
-     outfile << note << '\n\n'; 
+  outfile << get_current_time(); // time has trailing '\n' built-in
+  outfile << note << '\n\n';
 
-     if (outfile.fail())
-     {
-       std::cerr << "Failure writing to file: " << path << ".\n";
-       std::cerr << "This is usually caused by insufficient write permissions.\n"; 
-     }
+  if (outfile.fail())
+  {
+    std::cerr << "Failure writing to file: " << path << ".\n";
+    std::cerr << "This is usually caused by insufficient write permissions.\n";
+  }
 
-     outfile.close();
-
+  outfile.close();
 }
 
-git_commit * getLastCommit ( git_repository * repo )
+git_commit *get_last_commit(git_repository *repo)
 {
   int rc;
-  git_commit * commit = NULL; /* the result */
-  git_oid oid_parent_commit;  /* the SHA1 for last commit */
+  git_commit *commit = NULL; /* the result */
+  git_oid oid_parent_commit; /* the SHA1 for last commit */
 
   /* resolve HEAD into a SHA1 */
-  rc = git_reference_name_to_id( &oid_parent_commit, repo, "HEAD" );
-  if ( rc == 0 )
+  rc = git_reference_name_to_id(&oid_parent_commit, repo, "HEAD");
+  if (rc == 0)
   {
     /* get the actual commit structure */
-    rc = git_commit_lookup( &commit, repo, &oid_parent_commit );
-    if ( rc == 0 )
+    rc = git_commit_lookup(&commit, repo, &oid_parent_commit);
+    if (rc == 0)
     {
       return commit;
     }
   }
   return NULL;
 }
+
 
 /// Option Handling
 
@@ -81,16 +83,13 @@ bool cmdOptionExists(char **begin, char **end, const std::string &option)
 int main(int argc, char *argv[])
 {
 
-  // notepush::test();
-  // char *filename = getCmdOption(argv, argv + argc, "-f");
-
   if (cmdOptionExists(argv, argv + argc, "-h"))
   {
-    std::cout<<HEADER<<USAGE<<"\n";
+    std::cout << HEADER << USAGE << "\n";
     return 0;
   }
 
-    if (cmdOptionExists(argv, argv + argc, "-r"))
+  if (cmdOptionExists(argv, argv + argc, "-r"))
   {
     std::string answer;
     std::cout << "Are you sure you want to reset your configuration? Y or N\n";
@@ -128,9 +127,17 @@ int main(int argc, char *argv[])
     config.close();
   }
 
-
   /// load the options/config
   Options options;
+
+  if (cmdOptionExists(argv, argv + argc, "-o"))
+  {
+    options.online = true;
+    return 0;
+  }
+
+
+  
   std::ifstream config_file("/etc/notepush/psh.config");
   std::string line;
   while (std::getline(config_file, line))
@@ -138,20 +145,18 @@ int main(int argc, char *argv[])
     options.repo = std::move(line);
   }
 
-
-  //check to see if the user provided repo actually exists, if not, create it.
-  /* Open repository in given directory (or fail if not a repository) */
   git_libgit2_init();
 
   int error;
   git_repository *repo = NULL;
-  error = git_repository_open_ext(&repo, &options.repo[0], GIT_REPOSITORY_OPEN_NO_SEARCH, NULL);
 
+  /* Open repository in given directory (or fail if not a repository) */
+  error = git_repository_open_ext(&repo, &options.repo[0], GIT_REPOSITORY_OPEN_NO_SEARCH, NULL);
   if (error)
   {
     std::cerr << "Repository: " << options.repo << " is not a valid git repo.\n";
     std::cerr << "Please create a valid repository in the provided repo path.\n";
-    
+
     return 1;
   }
 
@@ -175,17 +180,11 @@ int main(int argc, char *argv[])
   //                         NULL, /* options, empty for defaults */
   //                         NULL); /* reflog mesage, usually "fetch" or "pull", you can leave it NULL for "fetch" */
 
-
-
-  // git_commit *head = getLastCommit(repo);
+  // git_commit *head = get_last_commit(repo);
 
   // const char *message = git_commit_message(head);
 
   // std::cout << message << "\n";
-
-
-  
-  
 
   /*
   The user provides a note with hashtags being used as the topical indicators. 
@@ -206,20 +205,120 @@ int main(int argc, char *argv[])
     if (argv[i][0] == '@')
     {
       topics.emplace(std::string(argv[i]));
-    } else
+    }
+    else
     {
       note << argv[i] << " ";
     }
   }
 
-  /// all notes get written to general. 
+  if (note.rdbuf()->in_avail() == 0)
+  {
+    std::cerr << "No note was provided\n";
+
+    return 0;
+  }
+
+  /// all notes get written to general.
   write_note(options.repo + "/general.txt", note.str());
 
   /// write notes to their respective files
-  for (const auto& topic : topics)
+  for (const auto &topic : topics)
   {
     write_note(options.repo + "/" + topic.substr(1) + ".txt", note.str());
   }
+
+  /// Git stuff
+
+  // Run the equivalent of "git add ."
+
+  // Get the git index.
+  git_index *index = NULL;
+  error = git_repository_index(&index, repo);
+  if (error != 0)
+    std::cerr << giterr_last()->message << std::endl;
+
+  // Add all files to the git index.
+  error = git_index_add_all(index, NULL, 0, NULL, NULL);
+  if (error != 0)
+    std::cerr << giterr_last()->message << std::endl;
+
+  // Write the index to disk.
+  error = git_index_write(index);
+  if (error != 0)
+    std::cerr << giterr_last()->message << std::endl;
+
+  // build a tree from the index
+  git_oid tree_oid;
+  git_index_write_tree(&tree_oid, index);
+
+  // create the commit
+  git_signature *signature;
+  git_signature_new(&signature, "Notepush", "notepush@fake.com", std::time(nullptr), 0);
+  git_tree *tree;
+  git_tree_lookup(&tree, repo, &tree_oid);
+
+  git_buf buffer;
+  memset(&buffer, 0, sizeof(git_buf));
+  git_message_prettify(&buffer, "Commit made via notepush", 0, '#');
+
+  // the parent is the most recent commit
+  git_commit *head = get_last_commit(repo);
+  git_oid commit_oid;
+  git_commit_create_v(
+      &commit_oid,
+      repo,
+      "HEAD",
+      signature,
+      signature,
+      NULL,
+      buffer.ptr,
+      tree,
+      1,
+      head);
+
+  if (options.online)
+  {
+    // git_remote *remote = NULL;
+    // if ((error = git_remote_lookup(&remote, repo, "origin")) != GIT_OK)
+    // {
+    //   std::cerr << "There was a problem getting the remote.\n";
+    //   goto done;
+    // }
+
+    // git_remote_connect(remote, GIT_DIRECTION_PUSH, NULL, NULL, NULL);
+
+    // git_remote_add_push(repo, "origin", "refs/heads/master:refs/heads/master");
+
+    //      // configure options
+    
+    //  git_push_options options;
+    //  git_push_init_options( &options, GIT_PUSH_OPTIONS_VERSION );
+
+    //  options.callbacks.credentials = git_cred_cb;
+
+    //  // do the push
+    //  git_remote_upload( remote, NULL, &options );
+
+    std::string command = "cd ";
+    command += options.repo;
+    command += "; git push origin master";
+
+    system (&command[0]);
+
+  }
+
+// Run "git status" to see the result.
+//system ("cd repository; git status");
+
+// Free resources.
+done:
+  git_buf_free(&buffer);
+  git_signature_free(signature);
+  git_tree_free(tree);
+  git_index_free(index);
+  git_repository_free(repo);
+  git_commit_free(head);
 
   return 0;
 }
